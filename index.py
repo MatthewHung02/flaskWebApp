a5cc20e7cde6e9b619f5c8232e9173895208ed5b
Matthew Hung <39603525+MatthewHung02@users.noreply.github.com>
2022-12-02 08:19:49 -0800

    fixed bug

--- index.py
+++ index.py
@@ -2,10 +2,17 @@ from flask import Flask
 from views import views
 
 
+
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
-nav = Navigation(app)
 
+
+
+# initializing Navigations
+# nav.Bar('top', [
+#     nav.Item('Home', 'index'),
+#     nav.Item('Gfg', 'gfg', {'page': 5}),
+# ])
 
 if __name__ == '__main__':
         app.run(debug=True)
