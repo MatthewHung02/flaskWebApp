62cb436001f7d46ec80952623b9f8d1b26cc7b43
Matthew Hung <39603525+MatthewHung02@users.noreply.github.com>
2022-12-07 22:59:52 -0800

    nav bar
    cant get nav bar working tonight :(

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from views import views
 
 
 
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/")
+
+@app.route('/')
+def index():
+  return render_template('index.html')
 
 
 
