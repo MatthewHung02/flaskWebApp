a898238f5ad2e7f46496ae378db4340ad0ccdbd1
Matthew Hung <39603525+MatthewHung02@users.noreply.github.com>
2022-12-09 17:20:06 -0800

    working links, main py runs now
    deploying to see if this clears up the 500 internal error

--- index.py
+++ index.py
@@ -3,11 +3,16 @@ from views import views
 from flask_bootstrap import Bootstrap
 
 
+
 app = Flask(__name__)
 Bootstrap(app)
 
 @app.route('/')
 def home():
+  return render_template('base.html')
+
+@app.route('/index')
+def index():
   return render_template('index.html')
 
 
