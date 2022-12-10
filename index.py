be4434711c80dabda931f9d547bf1386d4b357be
Matthew Hung <39603525+MatthewHung02@users.noreply.github.com>
2022-12-10 15:43:31 -0800

    changed folder structure

--- index.py
+++ index.py
@@ -1,29 +1,17 @@
 from flask import Flask, render_template
-from views import views
 from flask_bootstrap import Bootstrap
+from website import create_app
 
-
-
-app = Flask(__name__)
+app = create_app()
 Bootstrap(app)
 
-@app.route('/')
-def base():
-  return render_template('index.html')
+# @app.route('/')
+# def base():
+#   return render_template('index.html')
 
-@app.route('/aboutme')
-def aboutme():
-  return render_template('aboutme.html')
-
-
-
-
-
-# initializing Navigations
-# nav.Bar('top', [
-#     nav.Item('Home', 'index'),
-#     nav.Item('Gfg', 'gfg', {'page': 5}),
-# ])
+# @app.route('/aboutme')
+# def aboutme():
+#   return render_template('aboutme.html')
 
 if __name__ == '__main__':
         app.run(debug=True)
--- website/views.py
+++ website/views.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
 
-views = Blueprint(__name__, "views")
+views = Blueprint("views", __name__)
 
 
 @views.route("/")
 def home():
     return render_template("index.html")
 
+@views.route("/aboutme")
+def aboutme():
+    return render_template("aboutme.html")
