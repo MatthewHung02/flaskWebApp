67994a81d767f84716e584ac285952f1c1512957
Matthew Hung <39603525+MatthewHung02@users.noreply.github.com>
2023-11-03 18:35:47 -0700

    small stuff
    small stuff

--- index.py
+++ index.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+#from flask_bootstrap import Bootstrap
 from website import create_app
 
 app = create_app()
-Bootstrap(app)
+#Bootstrap(app)
 
-# @app.route('/')
-# def base():
-#   return render_template('index.html')
+@app.route('/')
+def base():
+  return render_template('index.html')
 
-# @app.route('/aboutme')
-# def aboutme():
-#   return render_template('aboutme.html')
+@app.route('/aboutme')
+def aboutme():
+  return render_template('aboutme.html')
+
+@app.route('/spotifychecker')
+def spotifychecker():
+  return render_template('spotifychecker.html')
 
 if __name__ == '__main__':
         app.run(debug=True)
--- website/views.py
+++ website/views.py
@@ -3,10 +3,11 @@ from flask import Blueprint, render_template
 views = Blueprint("views", __name__)
 
 
-@views.route("/")
+@views.route("/index")
 def home():
     return render_template("index.html")
 
 @views.route("/aboutme")
 def aboutme():
     return render_template("aboutme.html")
+#test test
