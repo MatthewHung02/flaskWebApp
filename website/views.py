6acf4099073e526fcdb00b3586348070553d3aae
Matthew Hung <39603525+MatthewHung02@users.noreply.github.com>
2023-12-21 20:58:21 -0800

    ?

--- website/views.py
+++ website/views.py
@@ -1,7 +1,6 @@
-'''
 from flask import Blueprint, render_template
 
-views = Blueprint("main", __name__)
+views = Blueprint("veiws", __name__)
 
 
 @views.route("/index")
@@ -15,4 +14,3 @@ def aboutme():
 @views.route("/spotifychecker")
 def spotifychecker():
     return render_template("spotifychecker.html")
-'''
