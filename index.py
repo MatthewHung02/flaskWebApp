57df45cba91fc9db58026330af0dd4c14da1ada3
Matthew Hung <39603525+MatthewHung02@users.noreply.github.com>
2023-12-25 11:55:59 -0800

    ?

--- index.py
+++ index.py
@@ -6,15 +6,15 @@ from flask import Flask, render_template
 from website import create_app
 app = create_app()
 
-@app.route('/')
+@app.route("/")
 def base():
   return render_template('index.html')
 
-@app.route('/aboutme')
+@app.route("/aboutme")
 def aboutme():
   return render_template('aboutme.html')
 
-@app.route('/spotifychecker')
+@app.route("/spotifychecker")
 def spotifychecker():
   return render_template('spotifychecker.html')
 
