a9a71e40cf12c767e7b10b2c98007f1fd80107ba
Matthew Hung <39603525+MatthewHung02@users.noreply.github.com>
2023-12-27 22:03:24 -0800

    added a few small things

--- views.py
+++ views.py
@@ -1,4 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request, session, jsonify
+from datetime import datetime, timedelta
+import urllib.parse
+import json
+import requests
+
+
+token_url = "https://accounts.spotify.com/api/token"
+base_url = "https://api.spotify.com/v1/"
+
+
+f = open('C:\\Users\\user\\Documents\\spotifycheckersecrets.txt')
+data = json.load(f)
+seatgeekid = data['sgid']
+seatgeeksecret = data['sgsecret']
+spotifyid = data['spotifyid']
+spotifysecrets = data['spotifysecret']
+
+
+scope = "user-top-read"
+type = "artists"
+term = "medium_term"
+spotifyclientid = "499455d664a942e78b3cdaa4f47774a5"
+
 
 views = Blueprint("veiws", __name__)
 
@@ -16,4 +39,58 @@ def aboutme():
 
 @views.route("/spotifychecker")
 def spotifychecker():
-    return render_template("spotifychecker.html")
+    if 'access_token' not in session:
+       return redirect('/login')
+    if datetime.now().timestamp > session['expires_at']:
+       return "Sorry, log back in"
+    
+    headers = {
+       'Authorization' : f"Bearer {session['access_token']}"
+    }
+
+    response = request.get(base_url + "me/top/artists", headers = headers)
+    artists = response.json()
+    return jsonify(artists)
+
+#render_template("spotifychecker.html")
+
+@views.route("/login")
+def login():
+    paramaters = {
+       'client_id':spotifyclientid,  
+       'type': type,
+       'time_range': term,
+       'show_dialog':True
+     }
+    
+    auth_url2 = f"https://accounts.spotify.com/authorize?{urllib.parse.urlencode(paramaters)}"
+    return redirect(auth_url2)
+
+@views.route("/callback")
+def callback():
+   redirect_url = "https://www.matthew-hung.com/"
+   if 'error' in request.args:
+      return jsonify({'error':request.args['error']})
+   if 'code' in request.args:
+      request_body = {
+         'code' : request.args['code'],
+         'grant_type' : 'authorization_code',
+         'redirect_uri' : redirect_url,
+         'client_id' : spotifyid,
+         'client_secret': spotifysecrets
+
+      }
+
+      response = requests.post(token_url, data=request_body)
+      tokeninfo = response.json()
+
+      session['access_token'] = tokeninfo['access_token']
+      session['refresh_token'] = tokeninfo['refresh_token']
+      session['expires_at'] = datetime.now().timestamp + tokeninfo['refresh_token']
+
+      return redirect('/spotifychecker')
+   
+
+
+
+
