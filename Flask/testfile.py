a428c9add6a0e09bbdc120c78d4cfad1ef4d153a
Matthew Hung <39603525+MatthewHung02@users.noreply.github.com>
2024-01-09 17:50:52 -0800

    ?
    changed the file structure. let's see if it still works?

---
+++ Flask/testfile.py
@@ -0,0 +1,732 @@
+spotifyjson = """{
+	"href": "https://api.spotify.com/v1/me/top/artists",
+	"items": [
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/7FU0xCgmSYQEiBeevUqQ4S"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"nyc pop"
+			],
+			"href": "https://api.spotify.com/v1/artists/7FU0xCgmSYQEiBeevUqQ4S",
+			"id": "7FU0xCgmSYQEiBeevUqQ4S",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5ebb64d506775e6e1eec482d540",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab67616100005174b64d506775e6e1eec482d540",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f178b64d506775e6e1eec482d540",
+					"width": 160
+				}
+			],
+			"name": "Yoke Lore",
+			"popularity": 60,
+			"type": "artist",
+			"uri": "spotify:artist:7FU0xCgmSYQEiBeevUqQ4S"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/45dkTj5sMRSjrmBSBeiHym"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"alt z",
+				"pop"
+			],
+			"href": "https://api.spotify.com/v1/artists/45dkTj5sMRSjrmBSBeiHym",
+			"id": "45dkTj5sMRSjrmBSBeiHym",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb846308bf9bf7d3183958b3a6",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab67616100005174846308bf9bf7d3183958b3a6",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f178846308bf9bf7d3183958b3a6",
+					"width": 160
+				}
+			],
+			"name": "Tate McRae",
+			"popularity": 86,
+			"type": "artist",
+			"uri": "spotify:artist:45dkTj5sMRSjrmBSBeiHym"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/6bmlMHgSheBauioMgKv2tn"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"sad lo-fi",
+				"sad rap"
+			],
+			"href": "https://api.spotify.com/v1/artists/6bmlMHgSheBauioMgKv2tn",
+			"id": "6bmlMHgSheBauioMgKv2tn",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb8e33a1f8941447de4926f4d7",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab676161000051748e33a1f8941447de4926f4d7",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f1788e33a1f8941447de4926f4d7",
+					"width": 160
+				}
+			],
+			"name": "Powfu",
+			"popularity": 68,
+			"type": "artist",
+			"uri": "spotify:artist:6bmlMHgSheBauioMgKv2tn"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/0bBz5bRBkExaej2HxtVfCw"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"sad rap"
+			],
+			"href": "https://api.spotify.com/v1/artists/0bBz5bRBkExaej2HxtVfCw",
+			"id": "0bBz5bRBkExaej2HxtVfCw",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb46fcd3768854ee87e8bb569e",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab6761610000517446fcd3768854ee87e8bb569e",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f17846fcd3768854ee87e8bb569e",
+					"width": 160
+				}
+			],
+			"name": "347aidan",
+			"popularity": 62,
+			"type": "artist",
+			"uri": "spotify:artist:0bBz5bRBkExaej2HxtVfCw"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/3ApUX1o6oSz321MMECyIYd"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"indie pop rap"
+			],
+			"href": "https://api.spotify.com/v1/artists/3ApUX1o6oSz321MMECyIYd",
+			"id": "3ApUX1o6oSz321MMECyIYd",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5ebb742526c1ecfba5c4bf03f91",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab67616100005174b742526c1ecfba5c4bf03f91",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f178b742526c1ecfba5c4bf03f91",
+					"width": 160
+				}
+			],
+			"name": "Quinn XCII",
+			"popularity": 62,
+			"type": "artist",
+			"uri": "spotify:artist:3ApUX1o6oSz321MMECyIYd"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/7pwufEBGfggjoI8twqlsmQ"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"alt z",
+				"social media pop"
+			],
+			"href": "https://api.spotify.com/v1/artists/7pwufEBGfggjoI8twqlsmQ",
+			"id": "7pwufEBGfggjoI8twqlsmQ",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb55d83e1420b367808a7c7b7f",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab6761610000517455d83e1420b367808a7c7b7f",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f17855d83e1420b367808a7c7b7f",
+					"width": 160
+				}
+			],
+			"name": "Nessa Barrett",
+			"popularity": 67,
+			"type": "artist",
+			"uri": "spotify:artist:7pwufEBGfggjoI8twqlsmQ"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/246dkjvS1zLTtiykXe5h60"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"dfw rap",
+				"melodic rap",
+				"pop",
+				"rap"
+			],
+			"href": "https://api.spotify.com/v1/artists/246dkjvS1zLTtiykXe5h60",
+			"id": "246dkjvS1zLTtiykXe5h60",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb6be070445b03e0b63147c2c1",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab676161000051746be070445b03e0b63147c2c1",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f1786be070445b03e0b63147c2c1",
+					"width": 160
+				}
+			],
+			"name": "Post Malone",
+			"popularity": 87,
+			"type": "artist",
+			"uri": "spotify:artist:246dkjvS1zLTtiykXe5h60"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/3Gm5F95VdRxW3mqCn8RPBJ"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"pop rap",
+				"portland hip hop",
+				"rap",
+				"underground hip hop"
+			],
+			"href": "https://api.spotify.com/v1/artists/3Gm5F95VdRxW3mqCn8RPBJ",
+			"id": "3Gm5F95VdRxW3mqCn8RPBJ",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb1f98760ed5b47b21eaf56e8f",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab676161000051741f98760ed5b47b21eaf56e8f",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f1781f98760ed5b47b21eaf56e8f",
+					"width": 160
+				}
+			],
+			"name": "Aminé",
+			"popularity": 66,
+			"type": "artist",
+			"uri": "spotify:artist:3Gm5F95VdRxW3mqCn8RPBJ"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/2RQXRUsr4IW1f3mKyKsy4B"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"pov: indie"
+			],
+			"href": "https://api.spotify.com/v1/artists/2RQXRUsr4IW1f3mKyKsy4B",
+			"id": "2RQXRUsr4IW1f3mKyKsy4B",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb7bfba04955b666b8b8219541",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab676161000051747bfba04955b666b8b8219541",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f1787bfba04955b666b8b8219541",
+					"width": 160
+				}
+			],
+			"name": "Noah Kahan",
+			"popularity": 84,
+			"type": "artist",
+			"uri": "spotify:artist:2RQXRUsr4IW1f3mKyKsy4B"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/6kDXH8d9LugUAsHIozzDAI"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"alt z"
+			],
+			"href": "https://api.spotify.com/v1/artists/6kDXH8d9LugUAsHIozzDAI",
+			"id": "6kDXH8d9LugUAsHIozzDAI",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5ebd081366d14bf70861d3ceb3b",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab67616100005174d081366d14bf70861d3ceb3b",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f178d081366d14bf70861d3ceb3b",
+					"width": 160
+				}
+			],
+			"name": "Rosie Darling",
+			"popularity": 56,
+			"type": "artist",
+			"uri": "spotify:artist:6kDXH8d9LugUAsHIozzDAI"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"permanent wave",
+				"pop"
+			],
+			"href": "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU",
+			"id": "4gzpq5DPGxSnKTe4SA8HAU",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb989ed05e1f0570cc4726c2d3",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab67616100005174989ed05e1f0570cc4726c2d3",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f178989ed05e1f0570cc4726c2d3",
+					"width": 160
+				}
+			],
+			"name": "Coldplay",
+			"popularity": 87,
+			"type": "artist",
+			"uri": "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/5sXaGoRLSpd7VeyZrLkKwt"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"indie folk",
+				"pop folk",
+				"stomp and flutter",
+				"stomp and holler"
+			],
+			"href": "https://api.spotify.com/v1/artists/5sXaGoRLSpd7VeyZrLkKwt",
+			"id": "5sXaGoRLSpd7VeyZrLkKwt",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb4528d0f9bb51b241561a16f3",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab676161000051744528d0f9bb51b241561a16f3",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f1784528d0f9bb51b241561a16f3",
+					"width": 160
+				}
+			],
+			"name": "Gregory Alan Isakov",
+			"popularity": 68,
+			"type": "artist",
+			"uri": "spotify:artist:5sXaGoRLSpd7VeyZrLkKwt"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/1XlLhtgJjC4ROQZilBZAvw"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"singer-songwriter pop"
+			],
+			"href": "https://api.spotify.com/v1/artists/1XlLhtgJjC4ROQZilBZAvw",
+			"id": "1XlLhtgJjC4ROQZilBZAvw",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5ebf38fac7a7aa7afa10a500a73",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab67616100005174f38fac7a7aa7afa10a500a73",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f178f38fac7a7aa7afa10a500a73",
+					"width": 160
+				}
+			],
+			"name": "Nic D",
+			"popularity": 66,
+			"type": "artist",
+			"uri": "spotify:artist:1XlLhtgJjC4ROQZilBZAvw"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/3QSQFmccmX81fWCUSPTS7y"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"australian pop",
+				"pop"
+			],
+			"href": "https://api.spotify.com/v1/artists/3QSQFmccmX81fWCUSPTS7y",
+			"id": "3QSQFmccmX81fWCUSPTS7y",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb4fc14e184f9e05ba0676ddee",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab676161000051744fc14e184f9e05ba0676ddee",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f1784fc14e184f9e05ba0676ddee",
+					"width": 160
+				}
+			],
+			"name": "Dean Lewis",
+			"popularity": 74,
+			"type": "artist",
+			"uri": "spotify:artist:3QSQFmccmX81fWCUSPTS7y"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/1ShZZUjkbXCjhwrb18BA8I"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"indie pop rap"
+			],
+			"href": "https://api.spotify.com/v1/artists/1ShZZUjkbXCjhwrb18BA8I",
+			"id": "1ShZZUjkbXCjhwrb18BA8I",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eba40b06f59b0afc23001e53c7",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab67616100005174a40b06f59b0afc23001e53c7",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f178a40b06f59b0afc23001e53c7",
+					"width": 160
+				}
+			],
+			"name": "Bryce Vine",
+			"popularity": 61,
+			"type": "artist",
+			"uri": "spotify:artist:1ShZZUjkbXCjhwrb18BA8I"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/2jnIB6XdLvnJUeNTy5A0J2"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"boy band",
+				"pop"
+			],
+			"href": "https://api.spotify.com/v1/artists/2jnIB6XdLvnJUeNTy5A0J2",
+			"id": "2jnIB6XdLvnJUeNTy5A0J2",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb9160fac7a1ecb8ee89a633ac",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab676161000051749160fac7a1ecb8ee89a633ac",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f1789160fac7a1ecb8ee89a633ac",
+					"width": 160
+				}
+			],
+			"name": "Why Don't We",
+			"popularity": 67,
+			"type": "artist",
+			"uri": "spotify:artist:2jnIB6XdLvnJUeNTy5A0J2"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/0eGAT5gCCSuMjddCUkGqeZ"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"kentucky indie"
+			],
+			"href": "https://api.spotify.com/v1/artists/0eGAT5gCCSuMjddCUkGqeZ",
+			"id": "0eGAT5gCCSuMjddCUkGqeZ",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb982742b7ad692d1fb2a99903",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab67616100005174982742b7ad692d1fb2a99903",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f178982742b7ad692d1fb2a99903",
+					"width": 160
+				}
+			],
+			"name": "Sugadaisy",
+			"popularity": 34,
+			"type": "artist",
+			"uri": "spotify:artist:0eGAT5gCCSuMjddCUkGqeZ"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/5MLIxVYkY4Fc2dwdaYSS8G"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"singer-songwriter pop"
+			],
+			"href": "https://api.spotify.com/v1/artists/5MLIxVYkY4Fc2dwdaYSS8G",
+			"id": "5MLIxVYkY4Fc2dwdaYSS8G",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb2332114b97a4ec44159147d6",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab676161000051742332114b97a4ec44159147d6",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f1782332114b97a4ec44159147d6",
+					"width": 160
+				}
+			],
+			"name": "Suriel Hess",
+			"popularity": 56,
+			"type": "artist",
+			"uri": "spotify:artist:5MLIxVYkY4Fc2dwdaYSS8G"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/5hPR4Atp3QY2ztiAcz1inl"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"folk-pop"
+			],
+			"href": "https://api.spotify.com/v1/artists/5hPR4Atp3QY2ztiAcz1inl",
+			"id": "5hPR4Atp3QY2ztiAcz1inl",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5ebe9e2b4cedbd5fce0d70e72f1",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab67616100005174e9e2b4cedbd5fce0d70e72f1",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f178e9e2b4cedbd5fce0d70e72f1",
+					"width": 160
+				}
+			],
+			"name": "Morningsiders",
+			"popularity": 52,
+			"type": "artist",
+			"uri": "spotify:artist:5hPR4Atp3QY2ztiAcz1inl"
+		},
+		{
+			"external_urls": {
+				"spotify": "https://open.spotify.com/artist/4MCBfE4596Uoi2O4DtmEMz"
+			},
+			"followers": {
+				"href": null,
+				"total": 0
+			},
+			"genres": [
+				"chicago rap",
+				"melodic rap",
+				"rap"
+			],
+			"href": "https://api.spotify.com/v1/artists/4MCBfE4596Uoi2O4DtmEMz",
+			"id": "4MCBfE4596Uoi2O4DtmEMz",
+			"images": [
+				{
+					"height": 640,
+					"url": "https://i.scdn.co/image/ab6761610000e5eb1908e1a8b79abf71d5598944",
+					"width": 640
+				},
+				{
+					"height": 320,
+					"url": "https://i.scdn.co/image/ab676161000051741908e1a8b79abf71d5598944",
+					"width": 320
+				},
+				{
+					"height": 160,
+					"url": "https://i.scdn.co/image/ab6761610000f1781908e1a8b79abf71d5598944",
+					"width": 160
+				}
+			],
+			"name": "Juice WRLD",
+			"popularity": 87,
+			"type": "artist",
+			"uri": "spotify:artist:4MCBfE4596Uoi2O4DtmEMz"
+		}
+	],
+	"limit": 20,
+	"next": "https://api.spotify.com/v1/me/top/artists?limit=20&offset=20",
+	"offset": 0,
+	"previous": null,
+	"total": 50
+}"""
+   
+listylist = []
+for each in spotifyjson['items']['name']:
+    print(spotifyjson['items']['name'])
+    #listylist.append(spotifyjson['items']['name'])
